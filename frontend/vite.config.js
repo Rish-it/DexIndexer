9812146e8cfa207f246004b495d617a108fde850
Rish-it <201b214@juetguna.in>
2025-03-30 19:19:10 +0530

    frontend fix

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            onwarn: function (warning, warn) {
+                // Ignore unused variable warnings
+                if (warning.code === 'UNUSED_EXTERNAL_IMPORT')
+                    return;
+                // Use default warning for everything else
+                warn(warning);
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    esbuild: {
+        logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    }
+});
